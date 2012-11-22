9f2b6dae3ced1632fe1be9087719597408e8e58e
Simon Booth <simon.booth@credit360.com>
2012-11-22 14:01:53 +0000

    Remove NSNulls from flattened arrays

---
+++ WorkflowSchema/Code/NSNull+WorkflowSchema.h
@@ -0,0 +1,15 @@
+//
+//  NSNull+WorkflowSchema.h
+//  WorkflowSchema
+//
+//  Created by Simon Booth on 22/11/2012.
+//  Copyright (c) 2012 CRedit360. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSNull (WorkflowSchema)
+
+- (NSArray *)flattenedArray;
+
+@end
---
+++ WorkflowSchema/Code/NSNull+WorkflowSchema.m
@@ -0,0 +1,18 @@
+//
+//  NSNull+WorkflowSchema.m
+//  WorkflowSchema
+//
+//  Created by Simon Booth on 22/11/2012.
+//  Copyright (c) 2012 CRedit360. All rights reserved.
+//
+
+#import "NSNull+WorkflowSchema.h"
+
+@implementation NSNull (WorkflowSchema)
+
+- (NSArray *)flattenedArray
+{
+    return @[];
+}
+
+@end
