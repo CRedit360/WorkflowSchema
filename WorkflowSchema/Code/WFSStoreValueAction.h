031c648d2e355436b9d8e1d7691e0b4d5f2d8386
Simon Booth <simon.booth@credit360.com>
2012-11-24 11:18:13 +0000

    Remove unused keyPath property

--- WorkflowSchema/Code/WFSStoreValueAction.h
+++ WorkflowSchema/Code/WFSStoreValueAction.h
@@ -10,8 +10,8 @@
 
 @interface WFSStoreValueAction : WFSAction
 
-@property (nonatomic, strong) NSString *name;    //Where to store the value
-@property (nonatomic, strong) id value;         // The value to store
-@property (nonatomic, strong) NSString *keyPath; // Where to get the value in the context (if value is not set)
+@property (nonatomic, strong) NSString *name; // Where to store the value
+@property (nonatomic, strong) id value;       // The value to store
+
 
 @end
